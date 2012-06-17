b87c6e662a12a36a10c72daa28fdd0b486f1be0e
cange <info@psykmedia.de>
2012-06-17 13:57:34 +0200

    wrapped Debug JavaScript into a Module

--- javascripts/lib/debug.js
+++ javascripts/lib/debug.js
@@ -1,8 +1,24 @@
-$(function () {
-
+var Debug = (function ($) {
   'use strict';
 
-   function sortByAlphabet(o) {
+  var module = {},
+    $panel = $(),
+    supportLocalStorage = !!window['localStorage'],
+    $html = $('html')
+  ;
+  module.options = {
+    colors: {
+      black: '#000',
+      white: '#fff'
+    },
+    storageKey: 'dev.domain.tld.debug'
+  };
+  var STORAGE_KEY = module.options.storageKey;
+
+  /**
+   * @private
+   */
+  function sortByAlphabet(o) {
     var sorted = {}, key, a = [];
 
     for (key in o) {
@@ -19,74 +35,96 @@ $(function () {
 
     return sorted;
   }
-
-  var colors = {
-      black: '#000',
-      white: '#fff'
-    },
-    $debugPanel = $()
-  ;
-
-  // sort by alphabet
-  colors = sortByAlphabet(colors);
-
+  /**
+   * @private
+   * @param {string} Color as a Hex, RGB, HLS value
+   * @param {string} Name of variable name in the preprocessor engine (SASS,
+   *   Less, etc.)
+   * @return {string} HTML div element with background-color and
+   *   color name as title attribute.
+   */
   function colorSquare(color, name) {
-    return '<div class="debug-color" style="background-color:'+color+'" title="$'+name+' '+color+'"></div>';
+    return '<div class="debug-color" style="background-color:' + color + '" title="$' + name + ' ' + color + '"></div>';
   }
-
+  /**
+   * @private
+   * @return {string} HTML list with all colors
+   */
   function buildColorSquares() {
-    var colorHTML = '';
-    for (var color in colors) {
-      colorHTML += '<li>' + colorSquare(colors[color], color) + '</li>';
+    var htmlItem = '',
+      colors = module.colors,
+      color;
+    // sort by alphabet
+    colors = sortByAlphabet(colors);
+
+    for (color in colors) {
+      if (colors.hasOwnProperty(color)) {
+        htmlItem += '<li>' + colorSquare(colors[color], color) + '</li>';
+      }
     }
-    return $('<ul class="debug-colors">' + colorHTML + '</ul>');
+    return '<ul class="debug-colors">' + htmlItem + '</ul>';
   }
-  // save current status of toggle state into the localStorage object
+  /**
+   * @private
+   * Save current status of toggle state into the localStorage object.
+   */
   function toggleHandler(event, options) {
     event.preventDefault();
     if (options.key) {
       $html.toggleClass('debug-' + options.key);
       if (supportLocalStorage) {
-        var currValue = localStorage.getItem(namespace) ? JSON.parse(localStorage.getItem(namespace)) : {},
+        var currValue = localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)) : {},
           value = {}
         ;
         value[options.key] = $html.hasClass('debug-' + options.key);
-        localStorage.setItem(namespace, JSON.stringify($.extend(currValue, value)));
+        localStorage.setItem(STORAGE_KEY, JSON.stringify($.extend(currValue, value)));
       }
     }
   }
-
-  // Creates a mouse sensitive debug panel on the top
-  if ($('.debug')[0]) {
-    var $debugPanel = $(''+
-      '<div id="debug-panel">'+
-        '<div class="inner">'+
-          '<span>Debug panel</span>'+
-          '<a class="debug-toggle" href="#toggle">toggle</a>'+
-          '<a class="debug-toggle-grid" href="#toggle">toggle Grid</a>'+
-          '<a href="static/styleguide">Styleguide</a>'+
-        '</div>'+
-      '</div>').prependTo('body');
-
-    var key = 'dev.domain.tld.debug',
-      supportLocalStorage = !!window['localStorage'],
-      $html = $('html')
-    ;
-    if (supportLocalStorage && localStorage.getItem(key) === 'true') {
-      $html.addClass('active');
+  /**
+   * @private
+   * Creates a mouse sensitive debug panel on the top
+   */
+  function buildPanel() {
+    if ($('.debug')[0]) {
+      $panel = $(''+
+        '<div id="debug-panel">'+
+          '<div class="inner">'+
+            '<span>Debug panel</span>'+
+            '<a class="debug-toggle-boxes" href="#toggle">Boxes</a>'+
+            '<a class="debug-toggle-grid" href="#toggle">Grid</a>'+
+            '<a href="static/styleguide">Styleguide</a>'+
+          '</div>'+
+        '</div>').prependTo('body');
     }
-    $debugPanel.find('.debug-toggle').click(function (event) {
-      event.preventDefault();
-      $html.toggleClass('active');
-      if (supportLocalStorage) {
-        localStorage.setItem(key, $html.hasClass('active'));
-      }
+  }
+
+  /**
+   * @public
+   * @constructor
+   */
+  module.init = function (options) {
+    $.extend(module.options, options || {});
+    buildPanel();
+
+    $panel.find('.inner').append(buildColorSquares());
+
+    $panel.find('.debug-toggle-boxes').click(function (event) {
+      toggleHandler(event, {key: 'boxes'});
     });
-    $debugPanel.find('.inner').append(buildColorSquares());
-    
-    $debugPanel.find('.debug-toggle-grid').click(function (event) {
+
+    $panel.find('.debug-toggle-grid').click(function (event) {
       toggleHandler(event, {key: 'grid'});
     });
-    
-  }
+
+  };
+
+  return module;
+
+}(jQuery));
+
+$(function () {
+  'use strict';
+
+  Debug.init();
 });
